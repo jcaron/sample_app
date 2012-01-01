d9a3b35933b0a715a34f6b4c77f5bc8bff3eee67
Joshua Caron <josh@tubedepot.com>
2012-01-01 17:27:28 -0600

    Added user followship

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,13 @@ class User < ActiveRecord::Base
   attr_accessible :name, :email, :password, :password_confirmation
 
   has_many :microposts, :dependent => :destroy
+  has_many :relationships,	:foreign_key => "follower_id",
+				:dependent => :destroy
+  has_many :following, :through => :relationships, :source => :followed
+  has_many :reverse_relationships, :foreign_key => "followed_id", 
+				   :class_name => "Relationship",
+				   :dependent => :destroy
+  has_many :followers, :through => :reverse_relationships, :source => :follower
 
   email_format = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
@@ -39,7 +46,19 @@ class User < ActiveRecord::Base
   end
 
   def feed
-    Micropost.where("user_id = ?", id)
+    Micropost.from_users_followed_by(self)
+  end
+
+  def following?(followed)
+    self.relationships.find_by_followed_id(followed)
+  end
+
+  def follow!(followed)
+    self.relationships.create!(:followed_id => followed.id)
+  end
+
+  def unfollow!(followed)
+    self.relationships.find_by_followed_id(followed).destroy
   end
 
   private
--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -8,20 +8,26 @@ describe PagesController do
   end
 
   describe "GET 'home'" do
-    it "should be successful" do
-      get 'home'
-      response.should be_success
-    end
+    describe "when not signed in" do
+      before(:each) do
+        get 'home'
+      end
+
+      it "should be successful" do
+        response.should be_success
+      end
       
-    it "should have the right title" do
-      get 'home'
-      response.should have_selector("title", 
+      it "should have the right title" do
+        response.should have_selector("title", 
 		:content => @base_title + " | Home")
+      end
     end
 
     describe "for signed in users" do
       before(:each) do
         @user = test_sign_in(Factory(:user))
+        other_user = Factory(:user, :email => Factory.next(:email))
+        other_user.follow!(@user)
       end
 
       it "should display the correct micropost count" do
@@ -50,6 +56,14 @@ describe PagesController do
 						:content => '2')
         response.should have_selector("a",	:href => "/?page=2",
 						:content => "Next")
+      end
+
+      it "should have the right follower/following counts" do
+        get 'home'
+        response.should have_selector('a', :href => following_user_path(@user),
+		:content => "0 following")
+        response.should have_selector('a', :href => followers_user_path(@user),
+		:content => "1 follower")
       end
     end
   end
